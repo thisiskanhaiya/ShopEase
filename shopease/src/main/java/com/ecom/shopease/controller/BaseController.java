1a2cccd5897f240487eb33d7a75d1c4dcbf29bd2
Kanhaiya Gawade <69666329+thisiskanhaiya@users.noreply.github.com>
2025-05-05 20:33:06 +0530

    initial-commit

---
+++ shopease/src/main/java/com/ecom/shopease/controller/BaseController.java
@@ -0,0 +1,13 @@
+package com.ecom.shopease.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BaseController {
+
+    @GetMapping("/home")
+    public String home() {
+        return "Welcome to Shopease!";
+    }
+}
